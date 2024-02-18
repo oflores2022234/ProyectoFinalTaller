15daa0cdef50181286eef561dcf5de5c1ae0bd5b
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-02-18 11:47:17 -0600

    realizamos route y controlador de login, y creamos el endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,20 +1,47 @@
-const { request, response } = require('express');
+const { request, response } = require("express");
 const Usuario = require("../models/usuario");
+const bycryptjs = require('bcryptjs');
 
 
 const login = async (req = request, res = response) => {
     const { correo, password } = req.body;
 
-    try {
-        
-    } catch (e) {
-        console.log(e);
-        res.status(500).json(){
-            msg
-        }
-    }
+    try{
+        const usuario = await Usuario.findOne({correo});
 
-}
+        if(!usuario){
+            return res.status(400).json({
+                msg: "Credencias incorrectas, correo no existe en la base de datos."
+            });
+        }
+
+         if(!usuario.estado){
+            return res.status(400).json({
+                msg: " El usuario no existe en la base de datos."
+            });
+         };
+
+         const validarPassword = bycryptjs.compareSync(password, usuario.password);
+         if(!validarPassword){
+            return res.status(400).json({
+                msg: "La contraseña es incorrecta"
+            })
+         }
+
+
+         res.status(200).json({
+            msg: "Bienvenido",
+            usuario
+         });
+
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            msg: "Comuniquese con el administrador"
+        });
+    };
+
+};
 
 module.exports = {
     login
