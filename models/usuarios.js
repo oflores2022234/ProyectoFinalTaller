727b053a1880944126519f4451db8d33229c4823
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-02-18 07:55:24 -0600

    finalizamos modelo usuarios

--- models/usuarios.js
+++ models/usuarios.js
@@ -1,10 +1,37 @@
 const { Schema, model } = require('mongoose');
 
+
 const UsuarioSchema = Schema({
 
     nombre:{
         type: String,
         require: [true, 'El nombre es obligatorio']
+    },
+    correo:{
+        type: String,
+        require: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    telefono:{
+        type: String,
+        require: [true, 'El numero de telefono es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        require: [true, 'La contraseña es obligatoria']
+    },
+    role:{
+        type: String,
+        require: true,
+        enum: ["ADMIN_ROLE", "USER_ROLE"]
+    },
+    estado:{
+        type: Boolean,
+        default: true
     }
 
-})
+
+});
+
+module.exports = model('Usuario', UsuarioSchema);
