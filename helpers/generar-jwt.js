86dedf710fab72a5e8b71a8df511e05db34d5097
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-02-18 12:15:41 -0600

    creamos el generador de jwt y creamos la llave secreta

---
+++ helpers/generar-jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = (uid = '') => {
+    return new Promise((resolve, reject) => {
+        const payload = { uid };
+        jwt.sign(
+            payload.env.SECRETORPRIVATEKEY,
+            {
+                expiresIn: '1h',
+            },
+            (err,token)=>{
+                err ? (console.log(err),reject('No se puede generar el token')): resolve(token);
+            }
+        );
+    });
+};
+
+module.exports = {
+    generarJWT
+}
