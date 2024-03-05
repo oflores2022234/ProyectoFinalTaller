bf2cd7a6602d8346decb6d3144a42b552a7acdc8
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-03-05 15:57:11 -0600

    add model of products

---
+++ src/productos/productos.model.js
@@ -0,0 +1,37 @@
+import mongoose, { mongo } from "mongoose";
+
+const ProductoSchema = mongoose.Schema({
+    nomnre: {
+        type: String,
+        required: [true, "The name of the product is obligatory"],
+    },
+    descripcion: {
+        type: String,
+        required: [true, "The description is obligatory"],
+    },
+    categoria: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Categoria',
+        required: true,
+    },
+    precio: {
+        type: Number,
+        required: [true, "the price is obligatory"],
+    },
+    stock: {
+        type: Number,
+        required: [true, "The stock is obligatory"],
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+ProductoSchema.methods.toJSON = function(){
+    const { __v, _id, ...producto} = this.toObject();
+    producto.uid = _id;
+    return producto;
+}
+
+export default mongoose.model('Producto', ProductoSchema);
