a080d82d46b6c7f8652e123572d12beaea2a20c3
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-03-05 13:49:03 -0600

    end the endpoints to categories

--- src/categorias/categorias.controller.js
+++ src/categorias/categorias.controller.js
@@ -11,3 +11,41 @@ export const categoriasPost = async (req, res) => {
         categoria
     });
 }
+
+export const categoriasGet = async (req = request, res = response) => {
+    const {limite, desde} = req.query;
+    const query = {estado: true};
+
+    const [total, categorias] = await Promise.all([
+        Categoria.countDocuments(query),
+        Categoria.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
+
+    res.status(200).json({
+        total,
+        categorias
+    });
+}
+
+export const categoriasPut = async (req, res = response) => {
+    const { id } = req.params;
+    const {_id, ...resto} = req.body;
+
+    await Categoria.findByIdAndUpdate(id, resto);
+
+    const categoria = await Categoria.findOne({_id: id});
+
+    res.status(200).json({
+        msg: "Categoty Update",
+        categoria
+    });
+};
+
+export const categoriasDelete = async (req, res) => {
+    const {id} = req.params;
+
+    const categoria = await Categoria.findByIdAndUpdate(id, { estado: false});
+    res.status(200).json({msg:'Categoria to delete', categoria});
+}
--- src/categorias/categorias.routes.js
+++ src/categorias/categorias.routes.js
@@ -2,17 +2,22 @@ import { Router } from "express";
 import { check } from "express-validator";
 
 import {
-    categoriasPost
+    categoriasPost,
+    categoriasGet,
+    categoriasPut,
+    categoriasDelete
 } from "./categorias.controller.js";
 
 import {
-    existenteCategoria
-    //existeCategoriaBI
+    existenteCategoria,
+    existeCategoriaById
 } from "../helpers/db-validators.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
-//import { validarJWT } from "../middlewares/validar-jwt.js";
+import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
+
+router.get("/", categoriasGet);
 
 router.post(
     "/",
@@ -21,5 +26,23 @@ router.post(
         check('descripcion', "The description is obligatory").not().isEmpty(),
         validarCampos,
     ], categoriasPost );
+
+router.put(
+    "/:id",
+    [
+        check("id", "Id no valid").isMongoId(),
+        check("id").custom(existeCategoriaById),
+        validarCampos,
+    ], categoriasPut
+);
+
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+        check("id", "Id no valid").isMongoId(),
+        check("id").custom(existeCategoriaById),
+        validarCampos,
+    ], categoriasDelete);
 
 export default router;
