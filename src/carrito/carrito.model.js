0a7de6a7bffff47ba2c7cc7479a48b34ebc5b710
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-03-07 21:20:40 -0600

    add model of carrito

--- src/carrito/carrito.model.js
+++ src/carrito/carrito.model.js
@@ -4,5 +4,18 @@ const CarritoSchema = mongoose.Schema({
     usuario: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Usuario',
-    }
-})
+        required: true
+    },
+    productos:[{
+        nombreProducto: {
+            type: String,
+            required: true
+        },
+        cantidad: {
+            type: Number,
+            required: true
+        },
+    }]
+});
+
+export default mongoose.model('Carrito', CarritoSchema);
