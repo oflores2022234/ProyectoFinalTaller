f5f4e62462dad5c55a41c1a0c1fe640f65e8875c
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-03-07 21:43:03 -0600

    add postCarrito

--- src/carrito/carrito.controller.js
+++ src/carrito/carrito.controller.js
@@ -1,8 +1,49 @@
-import mongoose from "mongoose";
+import Carrito from './carrito.model.js';	
+import Producto from '../productos/productos.model.js';
+import { validationResult } from 'express-validator';
 
-const CarritoSchema = mongoose.Schema({
-    usuario: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Usuario',
+export const carritoPost = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
-})
+
+    const { nombreProducto, cantidad } = req.body;
+    const usuario = req.usuario;
+
+    try {
+        const producto = await Producto.findOne({nombre: nombreProducto});
+
+        if(!producto){
+            return res.status(404).json({msg: 'Producto no encontrado'});
+        }
+
+        if(producto.stock < cantidad){
+            return res.status(400).json({msg: 'Stock insuficiente'});
+        }
+
+        let carrito = await Carrito.findOne({ usuario });
+
+        if(!carrito){
+            carrito = new Carrito({
+                usuario,
+                productos: []
+            });
+        }
+
+        const productoIndex = carrito.productos.findIndex(producto => producto.nombreProducto === nombreProducto);
+
+        if(!productoIndex !== -1){
+            carrito.productos[productoIndex].cantidad += cantidad;
+        }else{
+            carrito.productos.push({nombreProducto, cantidad});
+        }
+
+        await carrito.save();
+
+        res.status(201).json({msg: 'Producto agregado al carrito'});
+    } catch (error) {
+        console.log(`Error al agregar producto al carrito: `,error);
+        res.status(500).json({msg: 'Error al agregar producto al carrito'});
+    }
+}
